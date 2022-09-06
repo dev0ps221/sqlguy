5798cc8ff78a4eab3b1f98c6091865bda738e84b
dev0ps221 <seybaxscorpion@gmail.com>
2022-09-06 09:25:53 +0000

    login view | build server instance class for connection

---
+++ core/classes/ServerInstance.py
@@ -0,0 +1,11 @@
+
+
+class ServerInstance:
+    host = None
+    user = None
+    pwd = None
+
+    def __init__(self, host, user, pwd):
+        self.host = host
+        self.user = user
+        self.pwd  = pwd
