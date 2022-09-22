97ceab0df4f458074aa55df3f074eeeda3b914d1
dev0ps221 <seybaxscorpion@gmail.com>
2022-09-22 20:09:38 +0000

    server instance|cursor

--- core/classes/ServerInstance.py
+++ core/classes/ServerInstance.py
@@ -7,14 +7,21 @@ class ServerInstance:
     connected = False
     dbinstance = None
     cursor = None
+
     def connect(self):
         self.dbinstance = mysql.connector.connect(
             host=self.host,
             user=self.user,
             password=self.pwd
         )
+
+    def setcursor(self):
+        return self.cursor is not None
+
     def getcursor(self):
         if self.cursor is None:
+            self.cursor = self.dbinstance.cursor() if self.dbinstance is not None else self.cursor
+
     def delcursor(self):
         self.cursor = None
         print(self.dbinstance)
