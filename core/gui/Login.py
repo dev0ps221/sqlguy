50f69e4a6bebf27a3b0053469f4c11683ad069cb
dev0ps221 <seybaxscorpion@gmail.com>
2022-09-23 12:07:37 +0000

    iface|views

--- core/gui/Login.py
+++ core/gui/Login.py
@@ -13,24 +13,8 @@ class DatabasesView:
 
     def build_components(self):
         self.container = Container()
-        self.server_container = Container()
-        self.login_label_container = Container()
-        self.username_container = Container()
-        self.password_container = Container()
-        self.dologin_container = Container()
-        self.login_label = Text(value='SE CONNECTER A UN SERVEUR DE BASE DE DONNÉES')
-        self.server_input = TextField(label='server')
-        self.username_input = TextField(label='username')
-        self.password_input = TextField(label='password')
-        self.dologin_input = ElevatedButton(on_click=self.do_login,text='Se Connecter')
-        self.form = Column()
-        self.login_label_container.content = self.login_label
-        self.server_container.content = self.server_input
-        self.username_container.content = self.username_input
-        self.password_container.content = self.password_input
-        self.dologin_container.content = self.dologin_input
-        self.form.controls = [self.login_label_container,self.server_container,self.username_container,self.password_container,self.dologin_container]
-        self.container.content = self.form
+        self.databases_container = Container()
+        self.container.content =self.databases_container
 
     def update_form(self):
         self.form.update()
