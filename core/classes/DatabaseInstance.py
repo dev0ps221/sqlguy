4f0e6ca649e63e816697d9b7f8ce9c947c8529ab
dev0ps221 <seybaxscorpion@gmail.com>
2022-09-23 01:26:19 +0000

    table instance

--- core/classes/DatabaseInstance.py
+++ core/classes/DatabaseInstance.py
@@ -1,6 +1,22 @@
 class DatabaseInstance:
 
+    def query(self,query):
+        return self.server.executereq(query)
+
+    def use(self):
+        self.query(f"use {self.name}")
+
+    def process_databases(self,tables):
+        tbs = []
+        for db in tables:
+            tbs.append(DatabaseInstance(self,db))
+        return tbs
+
+    def setdata(self):
+        self.data = {'databases':self.process_databases(self.executereq('show databases'))}
+
     def __init__(self,serverinstance,raw):
-        print(serverinstance)
-        
+        self.server = serverinstance
+        self.name,self.rawerror = raw
+
     print(raw)  
