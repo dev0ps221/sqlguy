2c1ec708a22ee6fc1c466d52b85f68ff7f148472
dev0ps221 <seybaxscorpion@gmail.com>
2022-09-25 05:24:10 +0000

    iface|views|forms

--- core/classes/Form.py
+++ core/classes/Form.py
@@ -1,19 +1,30 @@
 
-class TextFieldElem:
-    data = {}
+class FieldElem:
     elem = None
+    def __init__(self,data):
+        self.data = data
+
     def build_elem(self):
         if 'type' in self.data:
-        
+            if self.data['type'] == 'text':
+                self.elem = TextFieldElem(self.data)
+
+class TextFieldElem:
+    data = {}
+    elem = Text() 
 
     def set_data(self,data):
         self.rawdata = data
         for key in data.keys():
             self.data[key] = key
 
+    def build_elem(self):
+        for key in self.data:
+            elem[:key] = self.data[:key]
+
     def __init__(self,data):
         self.set_data(data)
-    
+        self.build_elem()
 
 class FormView:
     field_elems = {}
