da434fef373ccb53266c5f89e7dd75cb198c57d5
dev0ps221 <seybaxscorpion@gmail.com>
2022-09-25 05:16:32 +0000

    iface|views|forms

---
+++ core/classes/Form.py
@@ -0,0 +1,17 @@
+
+class TextFieldElem:
+    
+    def set_data(self):
+        
+    def __init__(self,data):
+        self.set_data(data)
+    
+
+class FormView:
+    field_elems = {}
+
+    def build_field_elem(self,field):
+
+
+    def __init__(self,fields):
+        self.fields = fields
