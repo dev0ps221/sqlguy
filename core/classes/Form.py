45371be1939f069bab1f4838761c7a14c0914a2d
dev0ps221 <seybaxscorpion@gmail.com>
2022-09-25 05:19:28 +0000

    iface|views|forms

--- core/classes/Form.py
+++ core/classes/Form.py
@@ -1,8 +1,16 @@
 
 class TextFieldElem:
-    
-    def set_data(self):
+    data = {}
+    elem = None
+    def build_elem(self):
+        if 'type' in self.data:
         
+
+    def set_data(self,data):
+        self.rawdata = data
+        for key in data.keys():
+            self.data[key] = key
+
     def __init__(self,data):
         self.set_data(data)
     
