21a42dc1caee31b1a3473dcc340f8b6826593929
dev0ps221 <seybaxscorpion@gmail.com>
2022-09-26 20:16:10 +0000

    minimumui | list server databases | select database

--- core/gui/Databases.py
+++ core/gui/Databases.py
@@ -7,8 +7,10 @@ from core.classes.ServerInstance import ServerInstance
 class DatabasesView:
     server = None
     selected_database = None
+    selected_view = None
     databases_container_label=Text(value='DATABASES')
     databases = Row(scroll='adaptive')
+    databasesactions = Row(scroll='adaptive')
     databases_container = Column()
     container = Container(bgcolor=colors.BLUE_200)
 
@@ -27,10 +29,14 @@ class DatabasesView:
                 self.selected_database = database
         self.update()
 
+    def select_view(self,view):
+        self.selected_view = view
+        self.update()
+
     def getdatabases(self):
         return self.master.actual_server.getdatabases() if self.master.actual_server else []
 
-    def build_components(self):
+    def build_components_old(self):
         self.databases_container.controls = []
         self.databases.controls = []
         for database in self.getdatabases():
@@ -43,6 +49,17 @@ class DatabasesView:
             self.databases.controls.append(databasebutton)
         self.databases_container.controls.append(self.databases_container_label)
         self.databases_container.controls.append(self.databases)
+        self.container.content = self.databases_container
+
+    def build_components(self):
+        self.databases_container.controls = []
+        self.databasesactions.controls = []
+        def on_click(x):
+            self.select_view(x.control.text)
+        print(self.selected_view)
+        listdatabasebutton = ElevatedButton(text='list',on_click=on_click) 
+        self.databases_container.controls.append(self.databases_container_label)
+        self.databases_container.controls.append(self.databasesactions)
         self.container.content = self.databases_container
 
     def update_form(self):
